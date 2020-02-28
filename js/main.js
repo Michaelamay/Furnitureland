30b95648a6ea331de0ee7931a42d8f00053e849d
Michael Jonathan Amay <michaeljonathanamay@Michaels-MacBook-Air.local>
2020-02-28 12:29:55 -0600

    javascript folder

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+$('.slider-one')
+
+.not(".slick-intialized")
+.slick({
+    autoplay:true,
+    autoplaySpeed:3000,
+    dots:true,
+    prevArrow : ".site-slider .slider-btn .prev",
+    nextArrow : ".site-slider .slider-btn .next",
+});
+
+// slider two
+$('.slider-two')
+
+.not(".slick-intialized")
+.slick({
+
+    prevArrow : ".site-slider-two .prev",
+    nextArrow : ".site-slider-two .next",
+    slidesToShow:5,
+    slidesToScroll:1,
+    autoplaySpeed:3000
+});
+
+// slider three
+$('.slider-three')
+
+.not(".slick-intialized")
+.slick({
+
+    prevArrow : ".site-slider-three .prev",
+    nextArrow : ".site-slider-three .next",
+    slidesToShow:4,
+    slidesToScroll:1,
+    autoplaySpeed:3000,
+    infinite: false
+});
+
